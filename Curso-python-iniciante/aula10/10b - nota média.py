60c1952cea0ea4445fced3a38dc45ee5d2fdb54e
anajulianunes <anajulianuness@hotmail.com>
2020-07-30 13:05:51 -0300

    Pequena alteração - calculadora de média

--- Curso-python-iniciante/aula10/10b - nota média.py
+++ Curso-python-iniciante/aula10/10b - nota média.py
@@ -3,6 +3,6 @@ n2 = float(input('Qual sua segunda nota: '))
 m = (n1 + n2) / 2
 print('Sua média foi {:.1f}'.format(m))
 if m>=7:
-    print('vc passou cabaço')
+    print('Vc passou! Parabéns!!')
 else:
-    print('burro, tente de novo ano que vem')
+    print('ISH, tente de novo ano que vem, vc consegue')
