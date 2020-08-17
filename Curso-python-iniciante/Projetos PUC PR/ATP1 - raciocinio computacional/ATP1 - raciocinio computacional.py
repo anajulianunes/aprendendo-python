1171bd332b20ea2de41bcc304a4bd3928b664f79
anajulianunes <anajulianuness@hotmail.com>
2020-08-16 23:28:30 -0300

    Projetos em Python

---
+++ Curso-python-iniciante/Projetos PUC PR/ATP1 - raciocinio computacional/ATP1 - raciocinio computacional.py
@@ -0,0 +1,15 @@
+# Esta é a atividade prática da máteria de Raciocínio Computacional da PUC PR
+# Conforme pedido vamos iniciar mostrando meu nome completo e o nome da minha loja
+
+print("Olá! Bem-vindo à loja da Ana Julia Nunes de Araujo")
+
+# Em seguida pedirei os dados do cliente para iniciar a análise de crédito
+
+print("Vamos iniciar sua análise de crédito... ")
+cargo = str(input('Digite o seu cargo: '))
+salario = float(input('Digite o seu salário separando as casas decimais por ponto: '))
+ano = int(input('Digite o ano do seu nascimento: '))
+print('Você é {}, seu salário é de {} e você nasceu no ano {}'.format(cargo, salario, ano))
+
+
+
